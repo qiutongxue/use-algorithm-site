80ee351ab7ef28617ffe589463b221b61a9a5f51
berry <xqiutongxue@foxmail.com>
2023-01-20 22:37:50 +0800

    支持 container

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import remakrDirective from 'remark-directive';
+import remarkContainerPlugin from './scripts/remark-container-plugin';
+import remarkToc from 'remark-toc'
+import rehypeToc from '@jsdevtools/rehype-toc'
 
 // https://astro.build/config
-export default defineConfig({});
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [remakrDirective, remarkContainerPlugin],
+    rehypePlugins: [rehypeToc]
+  },
+  integrations: [mdx()]
+});
