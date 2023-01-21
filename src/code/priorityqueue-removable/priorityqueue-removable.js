9eaaf98c559832b2ca389a8e77616d18386da843
berry <xqiutongxue@foxmail.com>
2023-01-21 13:03:49 +0800

    开摆

---
+++ src/code/priorityqueue-removable/priorityqueue-removable.js
@@ -0,0 +1,50 @@
+"use strict";
+function useRemovablePriorityQueue(compare = (a, b) => a === b ? 0 : (a > b ? 1 : -1)) {
+    const pq = usePriorityQueue(compare);
+    const map = new Map();
+    let _size = 0;
+    const removeUnusable = () => {
+        while (!pq.isEmpty() && !map.has(pq.peek())) {
+            pq.pop();
+        }
+    };
+    const del = (e) => {
+        if (!map.has(e)) {
+            return false;
+        }
+        map.set(e, map.get(e) - 1);
+        if (map.get(e) <= 0) {
+            map.delete(e);
+        }
+        return true;
+    };
+    const push = (e) => {
+        pq.push(e);
+        map.set(e, (map.get(e) || 0) + 1);
+        _size++;
+    };
+    const peek = () => {
+        removeUnusable();
+        return pq.peek();
+    };
+    const pop = () => {
+        removeUnusable();
+        if (pq.isEmpty()) {
+            return null;
+        }
+        const res = pq.pop();
+        _size--;
+        del(res);
+        return res;
+    };
+    const remove = (e) => {
+        const hasElement = del(e);
+        if (hasElement) {
+            _size--;
+        }
+        return hasElement;
+    };
+    const size = () => _size;
+    const isEmpty = () => size() === 0;
+    return { push, pop, isEmpty, size, remove, peek };
+}
---
+++ src/pages/tools/priorityqueue-removable.md
@@ -0,0 +1,18 @@
+---
+layout: "../../layouts/mdlayout.astro"
+title: "可删除的优先队列"
+---
+
+# 优先队列（可删除元素）
+
+:::note{.warning}
+需要用到 usePriorityQueue，请到 [优先队列 PriorityQueue](./priorityqueue.md) 复制代码
+:::
+
+## JavaScript
+
+<<< @/../source/priorityqueue-removable/priorityqueue-removable.js
+
+## TypeScript
+
+<<< @/../source/priorityqueue-removable/priorityqueue-removable.ts
