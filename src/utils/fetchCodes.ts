1a0d0807e1f224aee7b14bd2a78a62e9d7f5e022
berry <xqiutongxue@foxmail.com>
2023-01-26 13:55:12 +0800

    调整 fetchCodes 结构

--- src/utils/fetchCodes.ts
+++ src/utils/fetchCodes.ts
@@ -1,5 +1,12 @@
 import { dirname, extname } from "path";
 
+interface CodeProps {
+    chName: string
+    enName: string
+    codes: any[]
+    keywords: string
+}
+
 const priority = [
     "ts",
     "js",
@@ -9,22 +16,15 @@ const priority = [
     "go"
 ]
 
-let cache: Record<string, any[]> | null = null
-let mapper: Record<string, string> | null = null
+let cache: Record<string, CodeProps> | null = null
 
 export async function fetchCodes() {
-    if (!cache) fetch()
+    if (!cache) await fetch()
     return cache
-}
-
-export async function fetchTitle() {
-    if (!mapper) await fetch()
-    return mapper
 }
 
 async function fetch() {
     cache = {}
-    mapper = {}
     const globs = import.meta.glob("/src/code/**/*", { as: "raw" });
 
     for (const [path, promise] of Object.entries<() => Promise<string>>(globs)) {
@@ -33,23 +33,26 @@ async function fetch() {
         const post = dirname(path).split("/").at(-1);
         if (!post || post === 'code') continue
 
+        const obj = cache[post] || (cache[post] = {} as CodeProps)
+
+        obj.enName ??= post.split('_').map(word => word.at(0)?.toUpperCase() + word.slice(1)).join(' ')
+
         if (!lang) {
-            const chName = path.split('/').at(-1) || post
-            mapper[post] = chName
+            const chName = path.split('/').at(-1) || obj.enName
+            obj.chName ??= chName
             continue
         }
         
-        const codes = cache[post] || (cache[post] = [])
+        const codes = obj.codes || (obj.codes = [])
         codes.push({
             code,
             lang
         })        
     }
 
-
-
-    for (const value of Object.values(cache)) {
-        value.sort((a, b) => priority.indexOf(a.lang) - priority.indexOf(b.lang))
+    for (const [post, props] of Object.entries(cache)) {
+        props.codes.sort((a, b) => priority.indexOf(a.lang) - priority.indexOf(b.lang))
+        props.keywords = `${props.enName} ${props.chName} ${post} ${props.enName.split(' ').map(s => s.at(0)).join('')}`.toLowerCase()
     }
 }
 
