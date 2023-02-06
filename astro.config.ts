c4e2116a1387afbde5fc022576564d416520e1a5
berry <xqiutongxue@foxmail.com>
2023-02-06 21:39:49 +0800

    支持数学公式

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'astro/config';
-import remakrDirective from 'remark-directive';
+import { defineConfig } from 'astro/config'
+import remakrDirective from 'remark-directive'
 import remarkContainerPlugin from './plugins/remark-container-plugin'
-import remarkTocPlugin from './plugins/rehype-toc-plugin';
-
+// import remarkTocPlugin from './plugins/rehype-toc-plugin';
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [remakrDirective, remarkContainerPlugin],
-    rehypePlugins: [remarkTocPlugin]
+    remarkPlugins: [remakrDirective, remarkContainerPlugin, remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
   integrations: [mdx()],
-});
+})
