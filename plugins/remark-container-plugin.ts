5fd08140de1717d151494b5166b4d940c8427933
berry <xqiutongxue@foxmail.com>
2023-02-11 18:57:40 +0800

    feat：支持 leetcode 题目链接

--- plugins/remark-container-plugin.ts
+++ plugins/remark-container-plugin.ts
@@ -20,26 +20,53 @@ export default function remarkContainerPlugin() {
   return (tree: Root) => {
     visit(tree, 'containerDirective', (node) => {
       if (node.type === 'containerDirective') {
-        if (node.name !== 'note')
-          return
-
-        const data = node.data ?? (node.data = {})
-        const attributes = node.attributes ?? (node.attributes = {})
-
-        const containerType = attributes.class
-        const tagName = containerType === 'details' ? 'details' : 'div'
-        attributes.class = `${containerType} custom-block`
-
-        data.hName = tagName
-        data.hProperties = h(tagName, attributes).properties
-
-        data.hChildren = [
-          // title
-          h('p', { class: 'custom-block-title' }, [containerType.toUpperCase()]),
-          // body
-          ...node.children.map(n => toHast(n)),
-        ]
+        if (node.name === 'note')
+          return handleNoteContainer(node)
+        if (node.name === 'leetcode')
+          return handleLeetcodeContainer(node)
       }
     })
   }
+}
+
+function handleLeetcodeContainer(node: any) {
+  const data = node.data ?? (node.data = {})
+  const attributes = node.attributes ?? (node.attributes = {})
+  attributes.class = 'leetcode-block'
+
+  const links = []
+  ;(function getLinks(node) {
+    if (node.type && node.type === 'link' && node.url.includes('leetcode')) {
+      links.push({
+        text: node.children[0].value,
+        src: node.url,
+      })
+      return
+    }
+    if (node.children)
+      node.children.forEach(child => getLinks(child))
+  })(node)
+
+  data.hName = 'div'
+  data.hProperties = h('div', attributes).properties
+  data.hChildren = links.map(link => h('a', { href: link.src, class: 'leetcode-item', target: '_blank' }, link.text))
+}
+
+function handleNoteContainer(node: any) {
+  const data = node.data ?? (node.data = {})
+  const attributes = node.attributes ?? (node.attributes = {})
+
+  const containerType = attributes.class
+  const tagName = containerType === 'details' ? 'details' : 'div'
+  attributes.class = `${containerType} custom-block`
+
+  data.hName = tagName
+  data.hProperties = h(tagName, attributes).properties
+
+  data.hChildren = [
+    // title
+    h('p', { class: 'custom-block-title' }, [containerType.toUpperCase()]),
+    // body
+    ...node.children.map(n => toHast(n)),
+  ]
 }
