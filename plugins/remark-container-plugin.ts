a2b35118ef1d4b2d0b1780ec0f2b73e3dda84dd0
berry <xqiutongxue@foxmail.com>
2023-02-11 17:20:29 +0800

    添加部分注释

--- plugins/remark-container-plugin.ts
+++ plugins/remark-container-plugin.ts
@@ -5,6 +5,17 @@ import { toHast } from 'mdast-util-to-hast'
 
 // const containerTypes = ["info", "warning", "danger", "tip", "details"];
 
+/**
+ *  使用方式： https://github.com/syntax-tree/mdast-util-directive#syntax-tree
+ *  1. type === 'textDirective':
+ *      :name[text]{attributes}
+ *  2. type === 'leafDirective':
+ *      ::name[text](attributes)
+ *  3. type === 'containerDirective':
+ *      :::name[firstLine]{attributes}
+ *        other content
+ *      :::
+ */
 export default function remarkContainerPlugin() {
   return (tree: Root) => {
     visit(tree, 'containerDirective', (node) => {
@@ -12,17 +23,20 @@ export default function remarkContainerPlugin() {
         if (node.name !== 'note')
           return
 
-        const data = node.data || (node.data = {})
-        node.attributes ??= {}
-        const containerType = node.attributes.class
+        const data = node.data ?? (node.data = {})
+        const attributes = node.attributes ?? (node.attributes = {})
+
+        const containerType = attributes.class
         const tagName = containerType === 'details' ? 'details' : 'div'
-        node.attributes.class = `${containerType} custom-block`
+        attributes.class = `${containerType} custom-block`
 
         data.hName = tagName
-        data.hProperties = h(tagName, node.attributes).properties
+        data.hProperties = h(tagName, attributes).properties
 
         data.hChildren = [
+          // title
           h('p', { class: 'custom-block-title' }, [containerType.toUpperCase()]),
+          // body
           ...node.children.map(n => toHast(n)),
         ]
       }
