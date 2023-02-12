85d423b130397861bfab5c64786349b44c02ac2d
berry <xqiutongxue@foxmail.com>
2023-02-13 01:04:52 +0800

    调整所有 Java 代码，用类包裹

--- src/code/fastpow/fastpow.java
+++ src/code/fastpow/fastpow.java
@@ -1,12 +1,18 @@
-long fastPow(long a, long b) {
-    // int MOD = 1000000007;
-    long base = a % MOD, res = 1;
-    while (b != 0) {
-        if ((b & 1) == 1) {
-            res = res * base % MOD;
+class FastPow {
+    static long calc(long a, long b, long MOD) {
+        long base = a % MOD, res = 1;
+        while (b != 0) {
+            if ((b & 1) == 1) {
+                res = res * base % MOD;
+            }
+            base = base * base % MOD;
+            b >>>= 1;
         }
-        base = base * base % MOD;
-        b >>>= 1;	
+        return res % MOD;
     }
-    return res % MOD;
+
+    static long calc(long a, long b) {
+        int MOD = (int) 1e9 + 7;
+        return calc(a, b, MOD);
+    }
 }
--- src/code/manacher/manacher.java
+++ src/code/manacher/manacher.java
@@ -1,32 +1,39 @@
-void manacher(String s) {
-    char[] c = new char[s.length() * 2 + 1];
-    int n = c.length, _i = 1;
-    c[0] = '#';
-    for (char ch : s.toCharArray()) {
-        c[_i++] = ch;
-        c[_i++] = '#';
-    }
-    int[] dp = new int[n];
-    for (int i = 0, l = 0, r = -1; i < n; i++) {
-        int k = i > r ? 1 : Math.min(dp[l + r - i], r - i + 1);
-        // 朴素的中心扩散法
-        while (0 <= i - k && i + k < n && c[i-k] == c[i+k]) k++;
-        dp[i] = k--;
-        // 更新 l,r
-        if (i + k > r) {
-            r = i + k;
-            l = i - k;
-        }
-    }
-}
-// 实际情况长度 = dp[i] - 1
-/* 求最长回文子串：
-for (int d : dp) res = Math.max(res, d - 1);
-*/
+class Manacher {
 
-/* 求所有回文串个数： 
-for (int d : dp) {
-	int t = d - 1;
-	res += t / 2 + t % 2;
+    static int[] manacher(String s) {
+        char[] c = new char[s.length() * 2 + 1];
+        int n = c.length, _i = 1;
+        c[0] = '#';
+        for (char ch : s.toCharArray()) {
+            c[_i++] = ch;
+            c[_i++] = '#';
+        }
+        int[] dp = new int[n];
+        for (int i = 0, l = 0, r = -1; i < n; i++) {
+            int k = i > r ? 1 : Math.min(dp[l + r - i], r - i + 1);
+            // 朴素的中心扩散法
+            while (0 <= i - k && i + k < n && c[i - k] == c[i + k])
+                k++;
+            dp[i] = k--;
+            // 更新 l,r
+            if (i + k > r) {
+                r = i + k;
+                l = i - k;
+            }
+        }
+        return dp;
+    }
+    // 实际情况长度 = dp[i] - 1
+    /*
+     * 求最长回文子串：
+     * for (int d : dp) res = Math.max(res, d - 1);
+     */
+
+    /*
+     * 求所有回文串个数：
+     * for (int d : dp) {
+     * int t = d - 1;
+     * res += t / 2 + t % 2;
+     * }
+     */
 }
-*/
