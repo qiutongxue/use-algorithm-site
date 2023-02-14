3471f2b635529a83e91e74491e4cd1eccbee0f3e
berry <xqiutongxue@foxmail.com>
2023-02-14 17:36:57 +0800

    format

--- src/code/gcd_lcm/gcd_lcm.rs
+++ src/code/gcd_lcm/gcd_lcm.rs
@@ -1,7 +1,11 @@
-fn gcd(a: i32, b: i32) -> i32{ 
-    if b == 0 { a } else { gcd(b, a % b) }
+fn gcd(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }
 
-fn lcm(a: i32, b: i32)  -> i32 {
+fn lcm(a: i32, b: i32) -> i32 {
     a * b / gcd(a, b)
 }
