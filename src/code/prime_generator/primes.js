c140e97c80d49b56cfe440f2d22719e8612829b1
berry <xqiutongxue@foxmail.com>
2023-02-28 13:33:55 +0800

    [primes][js] 新增

---
+++ src/code/prime_generator/primes.js
@@ -0,0 +1,13 @@
+"use strict";
+function primesIn(n) {
+    const vis = new Array(n + 1).fill(false);
+    const primes = [];
+    for (let i = 2; i <= n; i++) {
+        if (vis[i])
+            continue;
+        primes.push(i);
+        for (let j = i * i; j <= n; j += i)
+            vis[j] = true;
+    }
+    return primes;
+}
