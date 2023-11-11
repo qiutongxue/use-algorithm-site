29b2e16c8a66d6fc7741907511718d7bd4094312
berry <xqiutongxue@foxmail.com>
2023-11-11 13:27:04 +0800

    [uf]{rs} cleaning

--- src/code/union_find/union_find.rs
+++ src/code/union_find/union_find.rs
@@ -1,19 +1,14 @@
 struct UnionFind {
     parent: Vec<usize>,
     size: Vec<usize>,
-    groups: i32,
+    groups: usize,
 }
 
 impl UnionFind {
     fn new(n: usize) -> Self {
-        let parent = vec![0; n]
-            .iter()
-            .enumerate()
-            .map(|(i, _)| i)
-            .collect::<Vec<usize>>();
-
+        let parent = (0..n).collect::<Vec<usize>>();
         let size = vec![1; n];
-        Self { parent, size, groups: n as i32 }
+        Self { parent, size, groups: n }
     }
 
     fn find(&mut self, x: usize) -> usize {
@@ -30,9 +25,7 @@ impl UnionFind {
         }
         self.groups -= 1;
         if self.size[rx] > self.size[ry] {
-            let t = rx;
-            rx = ry;
-            ry = t;
+            std::mem::swap(&mut rx, &mut ry);
         }
         self.parent[rx] = ry;
         self.size[ry] += self.size[rx];
