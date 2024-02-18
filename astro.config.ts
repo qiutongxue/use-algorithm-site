e880d871d2e1e022310eb6e7c481c8ee6d4d4d1c
berry <xqiutongxue@foxmail.com>
2024-02-18 13:16:04 +0800

    chore: update modules

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config'
 import remakrDirective from 'remark-directive'
-import remarkContainerPlugin from './plugins/remark-container-plugin'
 // import remarkTocPlugin from './plugins/rehype-toc-plugin';
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
 // https://astro.build/config
 import mdx from '@astrojs/mdx'
+import remarkContainerPlugin from './plugins/remark-container-plugin'
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    extendDefaultPlugins: true,
     remarkPlugins: [remakrDirective, remarkContainerPlugin, remarkMath],
     rehypePlugins: [rehypeKatex],
   },
