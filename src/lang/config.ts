ac1f1cdc7feeaf825e4e05c14e84e38b1fcf3cc4
DinnerWithoutTomato <xqiutongxue@foxmail.com>
2025-08-12 22:14:58 +0800

    fix: 修复 python 代码丢失的问题

--- src/lang/config.ts
+++ src/lang/config.ts
@@ -1,36 +1,40 @@
 interface LanguageConfig {
-	name: "js" | "ts" | "rs" | "go" | "java" | "cpp" | "cs";
-	include: string[];
-	exclude?: string[];
+    name: 'js' | 'ts' | 'rs' | 'go' | 'java' | 'cpp' | 'cs' | 'py'
+    include: string[]
+    exclude?: string[]
 }
 
 export const config = [
-	{
-		name: "js",
-		include: ["./javascript/*.js"],
-	},
-	{
-		name: "ts",
-		include: ["./typescript/*.ts"],
-	},
-	{
-		name: "rs",
-		include: ["./rust/src/*.rs", "!./rust/src/lib.rs"],
-	},
-	{
-		name: "java",
-		include: ["./java/*.java"],
-	},
-	{
-		name: "go",
-		include: ["./go/*.go"],
-	},
-	{
-		name: "cpp",
-		include: ["./cpp/*.cpp"],
-	},
-	{
-		name: "cs",
-		include: ["./cs/*.cs"],
-	},
-] as const satisfies readonly LanguageConfig[];
+    {
+        name: 'js',
+        include: ['./javascript/*.js'],
+    },
+    {
+        name: 'ts',
+        include: ['./typescript/*.ts'],
+    },
+    {
+        name: 'rs',
+        include: ['./rust/src/*.rs', '!./rust/src/lib.rs'],
+    },
+    {
+        name: 'java',
+        include: ['./java/*.java'],
+    },
+    {
+        name: 'py',
+        include: ['./python/*.py'],
+    },
+    {
+        name: 'go',
+        include: ['./go/*.go'],
+    },
+    {
+        name: 'cpp',
+        include: ['./cpp/*.cpp'],
+    },
+    {
+        name: 'cs',
+        include: ['./cs/*.cs'],
+    },
+] as const satisfies readonly LanguageConfig[]
